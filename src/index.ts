e7ef46ceec810b09f72e40c85b8eafe7936fce40
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2022-05-10 11:43:04 +0200

    Add ExportString Alpha

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './Object/ObjectExtensions';
 export * from './Mapper/MapperExtensions';
 export * from './Date/DateExtensions';
 export * from './Execute/ExecutionExtensions';
+export * from './Module/ExportString';
