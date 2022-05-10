9935461b190e8b7a5e6063b620ba9c0db9c18cfc
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2022-05-10 11:42:28 +0200

    Remove build folder

--- build/Execute/ExecutionExtensions.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const executePS: (scriptPath: string) => void;
-export declare const executeCMD: (scriptPath: string) => void;
-//# sourceMappingURL=ExecutionExtensions.d.ts.map
--- build/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './Object/ObjectExtensions';
-export * from './Mapper/MapperExtensions';
-export * from './Date/DateExtensions';
-export * from './Execute/ExecutionExtensions';
-//# sourceMappingURL=index.d.ts.map
