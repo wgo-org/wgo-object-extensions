c94e4be4b11a5aae475c116d1601129d4d25cb3c
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2022-05-10 15:21:33 +0200

    Update InjectCjsCompiledModule

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+/**
+ * @exports Object Extensions
+ */
 export * from './Object/ObjectExtensions';
+/**
+ * @exports Object Mapper
+ */
 export * from './Mapper/MapperExtensions';
+/**
+ * @exports Date Extensions
+ */
 export * from './Date/DateExtensions';
+/**
+ * @exports Execution Extensions
+ */
 export * from './Execute/ExecutionExtensions';
+/**
+ * @exports Code Injection Extensions
+ */
 export * from './Module/injectCjsCompiledModule';
